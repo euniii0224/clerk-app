09debb7a8aa576268d8eeba8812094eb96ae89c6
WonJongengit --versiongit config --global user.email <won-20241@naver.com>
2025-10-14 17:48:26 +0900

    2st

---
+++ src/app/courses/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import courseData from '../api/courses/data.json'
+import { Course } from '@/types/course'
+import Courses from '@/components/Courses'
+
+export default function Coursespage() {
+  const courses: Course[] = courseData
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">Courses</h1>
+      <Courses courses={courses} />
+    </div>
+  )
+}
